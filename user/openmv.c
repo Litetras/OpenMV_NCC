1d8c656586fdb79d2eb0e858d19bce65d3faea74
Litetras <86386454+Litetras@users.noreply.github.com>
2023-03-30 12:41:42 +0800

    Rename user-openmv.c.c to user/openmv.c

--- user/openmv.c
+++ user/openmv.c
@@ -1,4 +1,4 @@
-/*��Ƶ�̳����� https://www.bilibili.com/video/BV1UL411V7XK?p=2&share_source=copy_web   �ǳƣ���ιιι*/
+/*视频教程链接 https://www.bilibili.com/video/BV1UL411V7XK?p=2&share_source=copy_web   昵称：海喂喂喂*/
 
 #include "openmv.h"
 #include "stm32f1xx.h"
@@ -18,10 +18,10 @@ u8 sendBuf[4];
 uint8_t uart3_rxbuff;
 
 u8 RoomNum, TargetNum;
-u8 TASK=1;    //���TASK���Դ����openmv����ֵopenmv�ϵ�FindTask������openmvģ��ƥ��Ĳ�ͬģʽ
+u8 TASK=1;    //这个TASK可以传输给openmv，赋值openmv上的FindTask来控制openmv模板匹配的不同模式
 
 
-char TargetRoom = 0;  //A, B, C, D, E, F, G, H;    //��˸��ַ���Ӧ�ŵ�ͼʵ�ʷ��䣬���������3��8�������ӦC-H
+char TargetRoom = 0;  //A, B, C, D, E, F, G, H;    //这八个字符对应着地图实际房间，里面的数字3—8会随机对应C-H
 
 u8 FindStartFlag;
 u16 FindTimeCount;
@@ -30,19 +30,19 @@ u16 FindTimeCount;
 void Openmv_Receive_Data(uint8_t com_data)
 {
 		uint8_t i;
-		static uint8_t RxCounter1=0;//����
+		static uint8_t RxCounter1=0;//计数
 		static uint16_t RxBuffer1[10]={0};
 		static uint8_t RxState = 0;	
 		static uint8_t RxFlag1 = 0;
 
-		if(RxState==0&&com_data==0x2C)  //0x2c֡ͷ
+		if(RxState==0&&com_data==0x2C)  //0x2c帧头
 		{
 			
 			RxState=1;
 			RxBuffer1[RxCounter1++]=com_data;  
 		}
 
-		else if(RxState==1&&com_data==0x12)  //0x12֡ͷ
+		else if(RxState==1&&com_data==0x12)  //0x12帧头
 		{
 			RxState=2;
 			RxBuffer1[RxCounter1++]=com_data;
@@ -52,28 +52,28 @@ void Openmv_Receive_Data(uint8_t com_data)
 		{
 			 
 			RxBuffer1[RxCounter1++]=com_data;
-			if(RxCounter1>=10||com_data == 0x5B)       //RxBuffer1��������,�������ݽ���
+			if(RxCounter1>=10||com_data == 0x5B)       //RxBuffer1接受满了,接收数据结束
 			{
 				RxState=3;
 				RxFlag1=1;
 				
-				 //���������,���е���RxCounter1 == 7��  7-5 = 2    openmv���͹�����һ�����ݰ���8��
+				 //正常情况下,运行到这RxCounter1 == 7？  7-5 = 2    openmv发送过来的一个数据包有8个
 				Num =          RxBuffer1[RxCounter1-5]; 
-				LoR =          RxBuffer1[RxCounter1-4];     //-1���� 1���ң�0��ʾ��û��ʶ���κ�����
+				LoR =          RxBuffer1[RxCounter1-4];     //-1是左， 1是右，0表示还没有识别到任何数字
 				Finded_flag =  RxBuffer1[RxCounter1-3];
 				FindTask =      RxBuffer1[RxCounter1-2];
 				
-				//RxCounter1-1��֡β
+				//RxCounter1-1是帧尾
 				
-				//greenLED_Toggle;    //�������Ƿ�������ݵ�,��ƽ��תһ����ɹ�����һ�����ݣ��������һ����˼
+				//greenLED_Toggle;    //用来看是否接收数据的,电平翻转一次则成功接收一个数据，跟下面的一个意思
 		  	GetOpenmvDataCount++;      
-				//������1���ڳɹ�������ٸ����ݰ��� ��Ҫ��1s�ӵ���ʱ�������֡��Խ��Խ׼ȷ����λ���Ļ�ƫ��ʹ���
-				//�����һ�½�����룬��openmv�����֡��ֱ�Ӵ�����
+				//用来看1秒内成功解码多少个数据包的 需要在1s钟的延时中清除，帧率越高越准确，个位数的话偏差就大了
+				//不如改一下解码代码，将openmv那里的帧率直接传过来
 				
 			}
 		}
 
-		else if(RxState==3)		//����Ƿ���ܵ�������־
+		else if(RxState==3)		//检测是否接受到结束标志
 		{
 				if(RxBuffer1[RxCounter1-1] == 0x5B)
 				{
@@ -83,24 +83,24 @@ void Openmv_Receive_Data(uint8_t com_data)
 							RxState = 0;
 						
 				}
-				else   //���մ���
+				else   //接收错误
 				{
 							RxState = 0;
 							RxCounter1=0;
 							for(i=0;i<10;i++)
 							{
-									RxBuffer1[i]=0x00;      //�����������������
+									RxBuffer1[i]=0x00;      //将存放数据数组清零
 							}
 				}
 		} 
 
-		else   //�����쳣
+		else   //接收异常
 		{
 				RxState = 0;
 				RxCounter1=0;
 				for(i=0;i<10;i++)
 				{
-						RxBuffer1[i]=0x00;      //�����������������
+						RxBuffer1[i]=0x00;      //将存放数据数组清零
 				}
 		}
 }
@@ -109,13 +109,13 @@ void Openmv_Receive_Data(uint8_t com_data)
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-  uint8_t tem;// ��������޷��ŵ�
+  uint8_t tem;// 这里的是无符号的
 	
-	//RedSignal_Toggle;   //�������Ƿ�������ݵ�, ����Ҫ��ʱ��ûЧ���Ļ��ʹ���������3���ж϶�û����
-  if(huart->Instance== USART3)     //����ֻ��������дUSART3
+	//RedSignal_Toggle;   //用来看是否接收数据的, 这里要随时都没效果的话就代表连串口3的中断都没进来
+  if(huart->Instance== USART3)     //这里只能这样大写USART3
   {
 		   
-		//RedSignal_Toggle;   //�������Ƿ�������ݵ�, ����Ҫ��ʱ��ûЧ���Ļ��ʹ���������3���ж϶�û����
+		//RedSignal_Toggle;   //用来看是否接收数据的, 这里要随时都没效果的话就代表连串口3的中断都没进来
 		
     tem=uart3_rxbuff;
     Openmv_Receive_Data(tem);
@@ -129,10 +129,10 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 void SendDataToOpenmv(void)
 {
 	u8 i;
-	//���Ϸ��͸�openmv �����ݵĴ��� (֡ͷ�� ģ��ƥ��ģʽѡ���־λ��ģʽ2����Ҫƥ������֣�֡β)   //����Ҫ�ܸߵķ���Ƶ��
+	//加上发送给openmv 的数据的代码 (帧头， 模板匹配模式选择标志位，模式2所需要匹配的数字，帧尾)   //不需要很高的发送频率
 		
 		
-		for(i = 0; i <= 4; i++)   //��TASK��TargetNum���һ���Է��͸�openmv
+		for(i = 0; i <= 4; i++)   //将TASK和TargetNum打包一次性发送给openmv
 		{
 			sprintf((char *)sendBuf, "*%d%d&", TASK, TargetNum);    
 				HAL_UART_Transmit(&huart3, sendBuf, sizeof(sendBuf), 1000);
@@ -142,24 +142,24 @@ void SendDataToOpenmv(void)
 }
 
 
-//�ȴ�openmvʶ��ָ������,������Ŀ�귿�䡣 ֻ���ڸ�λ��
+//等待openmv识别指定数字,并设置目标房间。 只有在复位后
 void SetTargetRoom(void)
 {
-		//��ѰĿ�겡���ŵĻ��������ݣ���ת����ʼ����   //�������һ�δ�openmv���������ּ�ΪĿ�귿��
-		/*һ��ʼʶ��Ŀ�귿���*/  
+		//查寻目标病房号的缓冲区数据，跳转任务开始函数   //重启后第一次从openmv传来的数字即为目标房号
+		/*一开始识别目标房间号*/  
 		if(Finded_flag == 1)
 		{
-			/**�˲�**/ //���Ե�û��Ҫ
+			/**滤波**/ //可以但没必要
 //			if(NumCount == 0)   
 //			{
 //				LastNum = Num;
 //			}
 //			
-//			else if(NumCount >= 2)  //������������һ���ģ����ж�Ϊʶ��
+//			else if(NumCount >= 2)  //至少连续两次一样的，才判断为识别到
 //			{
 //				if(LastNum == Num)
 //				{
-//					RoomNum = Num;    //�б�Ҫ�ö�һ��������Ҫ�˲����б�Ҫ,����ֱ����Num����
+//					RoomNum = Num;    //有必要用多一个变量吗？要滤波才有必要,否则直接用Num就行
 //					LastNum = 0;
 //					NumCount = 0;
 //				}
@@ -190,7 +190,7 @@ void SetTargetRoom(void)
 		 TASK = 2;  
 			
 	 }
-	 else if(RoomNum >= 3)  //����else if(3 <= Num <= 8)
+	 else if(RoomNum >= 3)  //不能else if(3 <= Num <= 8)
 	 {
 		 TargetRoom = 'G';
 		 TASK = 2;  
@@ -230,6 +230,6 @@ void SetTargetRoom(void)
 				TargetNum = 8;
 			break;	 		
 		}
-	 //ʶ�𵽵�������3-8�� Ĭ���ȸ�  TargetRoom = RoomH
-	 //����openmvʶ�𵽵�����,����ҩ�ĺ����������Ŀ��ֵ��ʵʱ����
+	 //识别到的数字是3-8， 默认先给  TargetRoom = RoomH
+	 //根据openmv识别到的数据,在送药的函数里面进行目标值的实时更改
 }
